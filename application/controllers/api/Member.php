8eea56f24fd21dbc4870886dd0f3e88950f075f4
kit rio <mario64aq@gmail.com>
2020-11-08 10:27:31 +0900

    Add login member controller

--- application/controllers/api/Member.php
+++ application/controllers/api/Member.php
@@ -21,10 +21,24 @@ class Member extends RestController
             'phoneNumber' => $this->post('phoneNumber')
         ];
         
-        if ($this->member->CreateMember($data) === 'duplication') {
+        if ($this->member->createMember($data) === 'duplication') {
             return $this->response('id 가 중복되었습니다.', 409);
         } else {
             return $this->response('success', 200);
         }
+    }
+
+    public function login_post()
+    {
+        $data = [
+            'memberid' => $this->post('memberid'),
+            'passwd' => $this->post('passwd'),
+        ];
+        if($this->member->loginMember($data)) {
+            return $this->response('success',200);
+        } else {
+            return $this->response('id또는 비밀번호가 틀렸습니다.',405);
+        }
+        
     }
 }
