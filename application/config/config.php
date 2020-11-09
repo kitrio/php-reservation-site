031fb3a28d9858b7f4f64292b8e5d39dc1b62e40
kit rio <mario64aq@gmail.com>
2020-11-09 14:26:24 +0900

    Change Cookie setting
    set httponly

--- application/config/config.php
+++ application/config/config.php
@@ -378,11 +378,11 @@ $config['encryption_key'] = '';
 |
 */
 $config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'ci_session';
+$config['sess_cookie_name'] = 'session';
 $config['sess_expiration'] = 7200;
 $config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
-$config['sess_time_to_update'] = 300;
+$config['sess_time_to_update'] = 600;
 $config['sess_regenerate_destroy'] = FALSE;
 
 /*
@@ -404,7 +404,7 @@ $config['cookie_prefix']	= '';
 $config['cookie_domain']	= '';
 $config['cookie_path']		= '/';
 $config['cookie_secure']	= FALSE;
-$config['cookie_httponly'] 	= FALSE;
+$config['cookie_httponly'] 	= TRUE;
 
 /*
 |--------------------------------------------------------------------------
