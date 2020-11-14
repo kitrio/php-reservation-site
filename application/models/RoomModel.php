5f3be12a9ec2dcd4ad0d38438959661b0da3bd3f
kit rio <mario64aq@gmail.com>
2020-11-14 22:15:56 +0900

    Fix room image loading

--- application/models/RoomModel.php
+++ application/models/RoomModel.php
@@ -10,12 +10,24 @@ class RoomModel extends CI_MODEL
 
     public function getAllRoominfo(): array
     {
-        return $this->db->get('room')->result_array();
+        $query = $this->db->get('room')->result_array();
+        $files = $this->db->get('file')->result_array();
+ 
+        foreach($query as $key => $room) {
+            foreach ($files as $file => $image) {
+                if ($room['room_number'] == $image['room_number']) {
+                    $query[$key]["image"][] = $files[$file];
+                }
+            }
+        }
+
+        return $query;
     }
 
     public function getRoominfoByNum(int $room_number): array
     {
         $this->db->where("room_number = $room_number");
+        
         return $this->db->get('room')->result_array();
     }
 
